a52503dfead870ae09b349bf16ebb5ec82446d0f
mrvic <mrvic.predrag@gmail.com>
2019-10-22 20:48:43 +0200

    dvostruka while petlja - ZADACA debugirati

--- 4.4-while-continue.php
+++ 4.4-while-continue.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Zadaca:
+ * Izbaciti iz rjesenja sve slucajeve kad je $x paran!!!
+ * Uz pomoc CONTINUE
+ */
 echo "<hr>Dvostruka petlja (tablica mnozenja)<br>";
 echo "Uz primjenu continue<br>";
 
@@ -10,13 +14,18 @@ while ($x <= 10) {
      if($x*$y>=40){
          break;
      }  
-
      echo " ".($x*$y);
+     
      $y++;
     }
     echo "<br>";
     $y = 1;
     $x++;  
-
+/*
+ *      if($x%2==0){
+         // break;
+         continue;
+     }
+ */
 
 }
